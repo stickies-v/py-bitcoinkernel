4103e2cb36b189fed5112b3461d7e673ee4b2533
stickies-v <stickies-v@protonmail.com>
2024-12-18 16:53:08 +0000

    validation: remove ValidationInterface
    Replace it with ContextOptions.set_validation_interface
    
    Addresses https://github.com/bitcoin/bitcoin/pull/30595#issuecomment-2505923802

--- src/pbk/validation.py
+++ src/pbk/validation.py
@@ -1,6 +1,5 @@
 import pbk.capi.bindings as k
 import pbk.util.callbacks
-from pbk.capi import KernelOpaquePtr
 
 
 class ValidationInterfaceCallbacks(k.kernel_ValidationInterfaceCallbacks):
@@ -15,10 +14,3 @@ default_validation_callbacks = ValidationInterfaceCallbacks(
         f"block_checked: block: {block}, state: {state}"
     ),
 )
-
-
-class ValidationInterface(KernelOpaquePtr):
-    def __init__(
-        self, callbacks: ValidationInterfaceCallbacks = default_validation_callbacks
-    ):
-        super().__init__(callbacks)
