523dee4273f1020c25695e81f62014178cc19205
stickies-v <stickies-v@protonmail.com>
2024-12-19 14:07:08 +0000

    chain: ChainstateManager automatically loads chainstate
    Addresses https://github.com/bitcoin/bitcoin/pull/30595#issuecomment-2553418201

--- test/test_chain.py
+++ test/test_chain.py
@@ -9,13 +9,14 @@ def test_chainstate_manager_options(temp_dir: Path):
     context = pbk.Context(opts)
     block_man_opts = pbk.BlockManagerOptions(context, str(temp_dir / "blocks"))
     chain_man_opts = pbk.ChainstateManagerOptions(context, str(temp_dir))
+    chain_load_opts = pbk.ChainstateLoadOptions()
 
     # valid number
     for num_threads in [0, 1, 5]:
         chain_man_opts.set_worker_threads_num(num_threads)
-        pbk.ChainstateManager(context, chain_man_opts, block_man_opts)
+        pbk.ChainstateManager(context, chain_man_opts, block_man_opts, chain_load_opts)
 
     # invalid numbers are automatically clamped between 0-15
     for num_threads in [-10, -1, 100]:
         chain_man_opts.set_worker_threads_num(num_threads)
-        pbk.ChainstateManager(context, chain_man_opts, block_man_opts)
+        pbk.ChainstateManager(context, chain_man_opts, block_man_opts, chain_load_opts)
