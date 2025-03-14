eea1fc186b20cdcbe0471547513f5684e0803767
stickies-v <stickies-v@protonmail.com>
2025-03-14 18:42:16 +0000

    log: add KernelLogViewer

--- test/test_log.py
+++ test/test_log.py
@@ -1,3 +1,7 @@
+import logging
+from datetime import datetime
+
+import pbk
 import pbk.log
 
 
@@ -30,3 +34,52 @@ def test_level_category():
     pbk.disable_log_category(
         pbk.LogCategory.BLOCKSTORAGE
     )  # Same operation twice should succeed
+
+
+def test_kernel_log_viewer(caplog):
+    caplog.set_level(logging.DEBUG)
+    logger = pbk.KernelLogViewer(name="test_logger", categories=[])
+    assert logger.getLogger() == logging.getLogger("test_logger")
+
+    time = "2025-03-19T12:14:55Z"
+    thread = "unknown"
+    filename = "context.cpp"
+    path = f"depend/bitcoin/src/kernel/{filename}"
+    lineno = 20
+    func = "operator()"
+    category = "all"
+    level = "info"
+    msg = "Using the 'arm_shani(1way,2way)' SHA256 implementation"
+    log_string = (
+        f"{time} [{thread}] [{path}:{lineno}] [{func}] [{category}:{level}] {msg}"
+    )
+    record = pbk.log.parse_kernel_log_string(logger.name, log_string)
+    assert record.name == logger.name
+    assert record.levelno == logging.INFO
+    assert record.levelname == level.upper()
+    assert record.filename == filename
+    assert record.pathname == path
+    assert record.lineno == lineno
+    assert record.msg == msg
+    assert record.threadName == thread
+    assert record.funcName == func
+    assert record.created == datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ").timestamp()
+
+    with logger.temporary_categories(categories=[pbk.LogCategory.KERNEL]):
+        assert logger.getLogger().getEffectiveLevel() == logging.DEBUG
+        try:
+            pbk.Block(bytes.fromhex("ab"))
+        except RuntimeError:
+            pass
+        assert caplog.records[-1].message == "Block decode failed."
+
+    debug_logger = pbk.KernelLogViewer(
+        name="debug_logger", categories=[pbk.LogCategory.KERNEL, pbk.LogCategory.PRUNE]
+    )
+    caplog.clear()
+    assert debug_logger.getLogger().getEffectiveLevel() == logging.DEBUG
+    try:
+        pbk.Block(bytes.fromhex("ab"))
+    except RuntimeError:
+        pass
+    assert caplog.records[-1].message == "Block decode failed."
