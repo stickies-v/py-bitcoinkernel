b3dd74fce32aaff2941572c83277751325fe26d1
stickies-v <stickies-v@protonmail.com>
2026-03-17 17:22:53 +0000

    style: address ruff format suggestions

--- src/pbk/notifications.py
+++ src/pbk/notifications.py
@@ -26,12 +26,10 @@ default_notification_callbacks = NotificationInterfaceCallbacks(
     header_tip=lambda user_data, state, height, timestamp, presync: print(
         f"header_tip: state: {state}, height: {height}, timestamp: {timestamp}, presync: {presync}"
     ),
-    progress=lambda user_data,
-    title,
-    title_len,
-    progress_percent,
-    resume_possible: print(
-        f"progress: title: {ctypes.string_at(title, title_len).decode('utf-8')}, progress_percent: {progress_percent}, resume_possible: {resume_possible}"
+    progress=lambda user_data, title, title_len, progress_percent, resume_possible: (
+        print(
+            f"progress: title: {ctypes.string_at(title, title_len).decode('utf-8')}, progress_percent: {progress_percent}, resume_possible: {resume_possible}"
+        )
     ),
     warning_set=lambda user_data, warning, message, message_len: print(
         f"warning_set: warning: {warning}, message: {ctypes.string_at(message, message_len).decode('utf-8')}"
